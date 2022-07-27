e105d31c343eca628177668094f7dd68ab0801e6
Sébastien Levert <slevert@outlook.com>
2022-07-27 19:29:51 -0400

    Adding webpack + bundle analyzer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import {
+    GraphClientInit
+} from "module-graph-sdk"
+
+import "module-graph-sdk/users/calendar";
+import "module-graph-sdk/me/calendar";
+import "module-graph-sdk/applications/application";
+
+async function  test() {
+    const client = GraphClientInit(process.env.ACCESS_TOKEN);
+    
+    const myEvents = await client.api("/me/calendar/events").get();
+    console.log(myEvents.value);
+
+    const applications = await client.api("/applications").get();
+    console.log(applications.value);
+}
+
+test();
 console.log("Hello Microsoft Graph!");
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Dotenv = require('dotenv-webpack');
+
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  target: "node",
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'bundle.html',
+        openAnalyzer: false
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
