410cd9d565b0f5a9265dbdb601d9d0f9514aee17
Sébastien Levert <slevert@outlook.com>
2022-07-27 19:41:17 -0400

    Adding call to add an event

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,23 @@ async function  test() {
 
     const applications = await client.api("/applications").get();
     console.log(applications.value);
+
+    const myEvent = await client.api("/me/calendar/events").post({
+        "subject": "Test Event",
+        "body": {
+            "contentType": "html",
+            "content": "A very important event from Graph"
+        },
+        "start": {
+            "dateTime": "2022-07-27T12:00:00",
+            "timeZone": "Eastern Standard Time"
+        },
+        "end": {
+            "dateTime": "2022-07-27T13:00:00",
+            "timeZone": "Eastern Standard Time"
+        },
+    });
+    console.log(myEvent);
 }
 
 test();
