cc8e7eb3b1f1f17551d3569eeca896c31bc3ddf7
Sébastien Levert <slevert@outlook.com>
2022-07-27 22:44:49 -0400

    Adding full preview SDK

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,32 @@
-import {
-    GraphClientInit
-} from "module-graph-sdk"
+import {GraphServiceClient, SimpleAuthenticationProvider} from "@microsoft/msgraph-sdk-javascript";
+import { DateTimeTimeZone, Event, ItemBody } from "@microsoft/msgraph-sdk-javascript/lib/src/models";
+import { BodyType } from "@microsoft/msgraph-sdk-javascript/lib/src/models/bodyType";
 
-import "module-graph-sdk/users/calendar";
-import "module-graph-sdk/me/calendar";
-import "module-graph-sdk/applications/application";
 
 async function  test() {
-    const client = GraphClientInit(process.env.ACCESS_TOKEN);
+    const authProvider = new SimpleAuthenticationProvider(async() => { return process.env.ACCESS_TOKEN! });
     
-    const myEvents = await client.api("/me/calendar/events").get();
-    console.log(myEvents.value);
+    const client = GraphServiceClient.init({authProvider});
+    
+    const myEvents = await client.me.calendar.events.get();
+    console.log(myEvents!.value);
 
-    const applications = await client.api("/applications").get();
-    console.log(applications.value);
+    const applications = await client.applications.get();
+    console.log(applications!.value);
 
-    const myEvent = await client.api("/me/calendar/events").post({
-        "subject": "Test Event",
-        "body": {
-            "contentType": "html",
-            "content": "A very important event from Graph"
-        },
-        "start": {
-            "dateTime": "2022-07-27T12:00:00",
-            "timeZone": "Eastern Standard Time"
-        },
-        "end": {
-            "dateTime": "2022-07-27T13:00:00",
-            "timeZone": "Eastern Standard Time"
-        },
-    });
-    console.log(myEvent);
+    var event = new Event();
+    event.subject = "Test Event"
+    event.body = new ItemBody();
+    event.body.content = "A very important event from Graph";
+    event.body.contentType = BodyType.Html;
+    event.start = new DateTimeTimeZone();
+    event.start.dateTime = "2022-07-27T12:00:00";
+    event.start.timeZone = "Eastern Standard Time";
+    event.end = new DateTimeTimeZone();
+    event.end.dateTime = "2022-07-27T13:00:00";
+    event.end.timeZone = "Eastern Standard Time";
+    const myEvent = await client.me.calendar.events.post(event);
+    console.log(myEvent!);
 }
 
 test();
