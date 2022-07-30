2b94bd4cb2a1ce67c59ea8b946338734c0613896
Sébastien Levert <slevert@outlook.com>
2022-07-29 21:49:04 -0400

    Easier management of bundle analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,15 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle.dev.html',
+      openAnalyzer: false
+    })
+  ],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle.prod.html',
+      openAnalyzer: false
+    })
+  ],
 });
