e831a3c80ac6fd15b9d470bcf6a4264931490f07
Sébastien Levert <slevert@microsoft.com>
2022-12-14 20:47:19 -0500

    adding comments

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,45 @@
-import {GraphServiceClient, SimpleAuthenticationProvider} from "@microsoft/msgraph-sdk-javascript";
-import { DateTimeTimeZone, Event, ItemBody } from "@microsoft/msgraph-sdk-javascript/lib/src/models";
+import {
+  GraphServiceClient,
+  SimpleAuthenticationProvider,
+} from "@microsoft/msgraph-sdk-javascript";
+import {
+  DateTimeTimeZone,
+  Event,
+  ItemBody,
+} from "@microsoft/msgraph-sdk-javascript/lib/src/models";
 import { BodyType } from "@microsoft/msgraph-sdk-javascript/lib/src/models/bodyType";
 
+async function test() {
+  console.log(process.env.ACCESS_TOKEN);
+  const authProvider = new SimpleAuthenticationProvider(async () => {
+    return process.env.ACCESS_TOKEN!;
+  });
 
-async function  test() {
-    console.log(process.env.ACCESS_TOKEN);
-    const authProvider = new SimpleAuthenticationProvider(async() => { return process.env.ACCESS_TOKEN! });
-    
-    const client = GraphServiceClient.init({authProvider});
-    
-    const myEvents = await client.me.calendar.events.get();
-    console.log(myEvents!.value);
+  const client = GraphServiceClient.init({ authProvider });
 
-    const applications = await client.applications.get();
-    console.log(applications!.value);
+  const myEvents = await client.me.calendar.events.get();
+  console.log(myEvents!.value);
 
-    var event = new Event();
-    event.subject = "Test Event"
-    event.body = new ItemBody();
-    event.body.content = "A very important event from Graph";
-    event.body.contentType = BodyType.Html;
-    event.start = new DateTimeTimeZone();
-    event.start.dateTime = "2022-07-27T12:00:00";
-    event.start.timeZone = "Eastern Standard Time";
-    event.end = new DateTimeTimeZone();
-    event.end.dateTime = "2022-07-27T13:00:00";
-    event.end.timeZone = "Eastern Standard Time";
-    const myEvent = await client.me.calendar.events.post(event);
-    console.log(myEvent!);
+  const applications = await client.applications.get();
+  console.log(applications!.value);
+
+  var event = new Event();
+  event.subject = "Test Event";
+  event.body = new ItemBody();
+  event.body.content = "A very important event from Graph";
+  event.body.contentType = BodyType.Html;
+  event.start = new DateTimeTimeZone();
+  event.start.dateTime = "2022-07-27T12:00:00";
+  event.start.timeZone = "Eastern Standard Time";
+  event.end = new DateTimeTimeZone();
+  event.end.dateTime = "2022-07-27T13:00:00";
+  event.end.timeZone = "Eastern Standard Time";
+  const myEvent = await client.me.calendar.events.post(event);
+  console.log(myEvent!);
+
+  client.users.get();
+
+  client.usersById("lkjaslkjdas").extensionsById("kjasdlkjasdlkj");
 }
 
 test().catch((e) => console.error(e));
