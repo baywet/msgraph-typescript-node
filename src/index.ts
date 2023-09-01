88041ad10ca13bb5bd2b0e1b41f22d45dbd1d4e3
Sébastien Levert <slevert@microsoft.com>
2023-09-01 18:10:11 +0000

    Added a default index.ts to reproduce

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello Microsoft Graph!");
+import "isomorphic-fetch";
+import { DeviceCodeCredential } from "@azure/identity";
+import { Client, ClientOptions } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+
+const credential = new DeviceCodeCredential({
+  clientId: "8863da3d-adbf-4a05-8f30-87bab9f76292",
+  userPromptCallback: (info) => {
+    console.log(info.message);
+  },
+});
+
+const authProvider = new TokenCredentialAuthenticationProvider(credential, {
+  scopes: ["User.Read"],
+});
+
+let clientOptions: ClientOptions = {
+  authProvider: authProvider,
+};
+
+const client = Client.initWithMiddleware(clientOptions);
+
+async function main(client: Client) {
+  const res = await client.api(`me`).get();
+  console.log(res);
+}
+
+main(client).catch((error) => {
+  console.log(error);
+});
