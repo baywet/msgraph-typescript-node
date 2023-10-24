6be14f9d91d92368dde195f71d6d4ff957fca477
Sébastien Levert <slevert@microsoft.com>
2023-10-24 15:05:39 +0000

    Adding the middleware proxy

---
+++ src/middlewares/ProxyMiddleware.ts
@@ -0,0 +1,27 @@
+import { Middleware, Context } from "@microsoft/msgraph-sdk-javascript";
+import { HttpsProxyAgent } from "hpagent";
+
+export class ProxyMiddleware implements Middleware {
+  private url: string;
+
+  private nextMiddleware!: Middleware;
+
+  public constructor(url: string = "http://localhost:8000") {
+    this.url = url;
+  }
+
+  public async execute(context: Context): Promise<void> {
+    if (context.options) {
+      context.options.agent = new HttpsProxyAgent({
+        proxy: this.url,
+        rejectUnauthorized: false,
+      });
+    }
+
+    return await this.nextMiddleware.execute(context);
+  }
+
+  public setNext(next: Middleware): void {
+    this.nextMiddleware = next;
+  }
+}
