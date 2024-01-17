1026e25b11e78dc4599daea6c19e4bf7eece339f
Vincent Biret <vibiret@microsoft.com>
2024-01-17 16:22:00 -0500

    - working demo
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { FetchRequestAdapter } from '@microsoft/kiota-http-fetchlibrary';
-import { GraphBaseServiceClient } from '@microsoft/msgraph-sdk-javascript/src/graphBaseServiceClient';
+import { newGraphServiceClient } from '@microsoft/msgraph-sdk-javascript';
+import '@microsoft/msgraph-sdk-javascript-appCatalogs';
+import '@microsoft/msgraph-sdk-javascript-admin';
 import { AnonymousAuthenticationProvider } from '@microsoft/kiota-abstractions';
 
 const fetchRequestAdapter = new FetchRequestAdapter(new AnonymousAuthenticationProvider());
 
 async function main() {
-  const graphClient = new GraphBaseServiceClient(fetchRequestAdapter);
-  const catalogs = await graphClient.appCatalogs.get();
+  const graphClient = newGraphServiceClient(fetchRequestAdapter);
+  // const appCatalogClient = newAppCatalogsServiceClient(fetchRequestAdapter);
+  // appCatalogClient.appCatalogs.get();
+  const catalogs = await graphClient.appCatalogs.teamsApps.get();
+  const admins = await graphClient.admin.get();
   console.log(catalogs);
+  console.log(admins);
 }
 
 main().catch(e => console.error(e));
